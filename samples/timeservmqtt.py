9ca2289bf618f0c0bd05848ef9c4da4fc69417a9
AbhinandSuresh <86036131+AbhinandSuresh@users.noreply.github.com>
2021-06-29 23:50:57 +0530

    Update timeservmqtt.py
    print unix time: final

--- samples/timeservmqtt.py
+++ samples/timeservmqtt.py
@@ -3,13 +3,13 @@ import time
 import calendar
 x=1
 import paho.mqtt.client as mqtt #import the client1
-broker_address="loccalhost"
+broker_address="localhost"
 broker_port=2020
 client = mqtt.Client("P1") #create new instance
 client.connect(broker_address,broker_port) #connect to broker
 while True:
 	d= datetime.utcnow()
 	unixtime = calendar.timegm(d.utctimetuple())
-	client.publish("house",unixtime)
+	client.publish("time",unixtime)
 	time.sleep(1)
 

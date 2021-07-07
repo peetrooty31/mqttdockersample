c8569f55d42877512edcda14f2538a560a7f2c46
sujithnair1 <sujith.nair711@gmail.com>
2021-07-07 13:19:35 -0400

    python program ssl changes

--- timeservmqtt.py
+++ timeservmqtt.py
@@ -1,19 +1,15 @@
 from datetime import datetime
 import time
 import calendar
-
-import paho.mqtt.client as mqtt #import the client1
+import ssl
+import paho.mqtt.client as mqtt
 broker_address="localhost"
-broker_port=2020
-client = mqtt.Client("P1") #create new instance
-client.connect(broker_address,broker_port) #connect to broker
+broker_port=8883
+client = mqtt.Client("P1") 
+client.tls_set(ca_certs="../ca.crt.pem", certfile="../client.crt.pem", keyfile="../client.key.pem", cert_reqs=ssl.CERT_REQUIRED,tls_version=ssl.PROTOCOL_TLS, ciphers=None)
+client.connect(broker_address,broker_port) 
 while True:
 	d= datetime.utcnow()
 	unixtime = calendar.timegm(d.utctimetuple())
-
 	client.publish("timeserver",unixtime)
-
-
-
 	time.sleep(1)
-
